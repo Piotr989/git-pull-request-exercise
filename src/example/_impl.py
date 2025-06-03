8f839686de2df4d7bed9dfa3c519b1a2b3de0b66
Grzegorz Bokota <bokota+github@gmail.com>
2025-06-03 19:40:54 +0200

    add some typing

--- src/example/_impl.py
+++ src/example/_impl.py
@@ -1,3 +1,8 @@
+from typing import Callable, Iterable, List, TypeVar
+
+T = TypeVar('T')
+V = TypeVar('V')
+
 def response_on_ultimate_question():
     """
     This function returns a response to the ultimate question of life, the universe, and everything.
@@ -5,7 +10,9 @@ def response_on_ultimate_question():
     return 42
 
 
-def calculate_func_on_interval(func, start, end, num_points=100):
+def calculate_func_on_interval(
+    func: Callable[[float], T], start: float, end: float, num_points: int=100
+) -> List[T]:
     """This function calculates the sum of a function evaluated at discrete points in a given interval.
     """
     diff = end - start
@@ -13,7 +20,7 @@ def calculate_func_on_interval(func, start, end, num_points=100):
     return [func(start + i * step) for i in range(num_points)]
 
 
-def apply(func, iterable):
+def apply(func: Callable[[T], V], iterable: Iterable[T]) -> List[V]:
     """This function applies a given function to each element in an iterable and returns a list of results.
     """
     return [func(x) for x in iterable]
